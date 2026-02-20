78c44dcc4c3429b1eeb44639e4c29060786ec874
Lisha from Black-Windows10-HP <lishastudies.cs@gmail.com>
2026-02-20 14:07:25 +0530

    Sidebar blueprint & orders.js

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+   {
+      product: 'Sunrise on the Reaping',
+      id: '92318',
+      paymentStatus: 'Paid',
+      status: 'Active'
+   },
+   {
+      product: 'The Iliad',
+      id: '32523',
+      paymentStatus: 'Due',
+      status: 'Pending'
+   },
+   {
+      product: 'My History Textbook',
+      id: '23503',
+      paymentStatus: 'Refunded',
+      status: 'Declined'
+   }
+]
